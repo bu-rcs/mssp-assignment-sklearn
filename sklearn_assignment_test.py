3b84520f85fe5a43602084746d59e60663091eff
sladenheim <saladenh@gmail.com>
2024-04-04 13:32:33 -0400

    Update sklearn_assignment_test.py for less stringent tests

--- sklearn_assignment_test.py
+++ sklearn_assignment_test.py
@@ -7,19 +7,9 @@ def test_problem1():
     df = ska.problem1("dataset/2022-2023_NBA_Player_Stats_Regular_Allstar.csv", 
                       "dataset/2023-2024_NBA_Player_Stats_Regular.csv", 
                       42)
-    expected_columns = ['Player', 
-                        'PTS', 
-                        'TRB', 
-                        'AST', 
-                        'STL', 
-                        'BLK', 
-                        'TOV',
-                        'Allstar Prediction']
     
     assert(isinstance(df, pd.DataFrame))
     assert(len(df) == 522)
-    assert(df.columns[0] == expected_columns[0])
-    assert(df.columns[-1] == expected_columns[-1])
     assert(df["Player"].loc[1] == "Bam Adebayo")
     assert(df["PTS"].loc[1] == pytest.approx(22.1, abs=0.5))
     assert(df["Allstar Prediction"].loc[1] == 1)
@@ -30,12 +20,7 @@ def test_problem2():
                           "dataset/2023-2024_NBA_Player_Stats_Regular.csv", 
                           "dataset/2023_2024_Allstars.csv",
                           42)
-    expected_keys= ['Not Allstar', 
-                    'Allstar', 
-                    'accuracy', 
-                    'macro avg', 
-                    'weighted avg']
+    
     assert(isinstance(report, dict))
-    assert([*report] == expected_keys)
     assert(report["Allstar"]["precision"] == pytest.approx(0.61, abs=0.05))
     assert(report["accuracy"] == pytest.approx(0.97, abs=0.03))
